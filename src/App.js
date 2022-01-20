b8fa65f3acc078781fcfcf2d7c124683a4a415f7
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-01-20 14:57:07 +0700

    add loading

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import React, { useState, useEffect, useRef } from "react";
 import NET from "vanta/dist/vanta.net.min";
 import * as THREE from "three";
+import load from "./assets/loading.gif";
 
 function App() {
   const [Quotes, setQuotes] = useState("");
   const [Author, setAuthor] = useState("");
+  const [loading, setLoading] = useState(false);
 
+  /* --------------------------------- VANTAJS -------------------------------- */
   const [vantaEffect, setVantaEffect] = useState(0);
   const myRef = useRef(null);
 
@@ -19,8 +22,8 @@ function App() {
           // mouseControls: true,
           // touchControls: true,
           // gyroControls: false,
-          // minHeight: 200.0,
-          // minWidth: 200.0,
+          minHeight: 300.0,
+          minWidth: 300.0,
           // scale: 1.0,
           // scaleMobile: 1.0,
           // backgroundColor: 0x1f1f3c,
@@ -36,6 +39,7 @@ function App() {
     getQuote();
   }, []);
 
+  /* -------------------------------- FETCH API ------------------------------- */
   const getQuote = () => {
     /* ----------------------------------- OLD API---------------------------------- */
     // fetch("http://quotes.stormconsultancy.co.uk/random.json")
@@ -47,11 +51,15 @@ function App() {
     //     setAuthor(data.author);
     //   });
 
+    setLoading(true);
     fetch("https://type.fit/api/quotes")
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
+        setTimeout(() => {
+          setLoading(false);
+        }, 300);
         const index = Math.round(Math.random() * (data.length - 1));
         setQuotes(data[index].text);
         setAuthor(data[index].author);
@@ -61,12 +69,26 @@ function App() {
   return (
     <div className="container" ref={myRef}>
       <div id="content" className="content">
-        <p id="quote" style={{ textAlign: "center" }}>
-          {Quotes}
-        </p>
-        <p id="author" className="author">
-          {Author}
-        </p>
+        {loading ? (
+          <div className="loadingio-spinner-dual-ring-q5h6gsnh5uf">
+            <div className="ldio-2u06t6977yh">
+              <div></div>
+              <div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <>
+            <q id="quote" style={{ textAlign: "center" }}>
+              {Quotes}
+            </q>
+            <p id="author" className="author">
+              - {Author} -
+            </p>
+          </>
+        )}
+
         <button className="btn" onClick={() => getQuote()}>
           New Quote
         </button>

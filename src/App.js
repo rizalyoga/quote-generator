f8d452a2817fb6a797a69c6f9aa17f6fb2774fd5
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-01-24 15:30:31 +0700

    add share button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useState, useEffect, useRef } from "react";
 import NET from "vanta/dist/vanta.net.min";
 import * as THREE from "three";
+import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon, WhatsappShareButton, WhatsappIcon, TelegramShareButton, TelegramIcon } from "react-share";
 
 function App() {
   const [Quotes, setQuotes] = useState("");
@@ -26,9 +27,9 @@ function App() {
           // scale: 1.0,
           // scaleMobile: 1.0,
           // backgroundColor: 0x1f1f3c,
-          points: 20.0,
-          maxDistance: 26.0,
-          spacing: 13.0,
+          // points: 20.0,
+          // maxDistance: 26.0,
+          // spacing: 13.0,
         })
       );
     }
@@ -69,7 +70,7 @@ function App() {
   };
 
   return (
-    <div className="container" ref={myRef}>
+    <div className="container container-content" ref={myRef}>
       <div id="content" className="content">
         {loading ? (
           <div className="loadingio-spinner-dual-ring-q5h6gsnh5uf">
@@ -94,6 +95,23 @@ function App() {
         <button className="btn" onClick={() => getQuote()}>
           New Quote
         </button>
+      </div>
+      <div className="share-btn">
+        <FacebookShareButton style={{ paddingLeft: "5px" }} url={`otesapps-rizalyoga.netlify.app`} quote={`${Quotes} \n -${Author}- \n`} hashtag="quotes">
+          <FacebookIcon size={36} round />
+        </FacebookShareButton>
+
+        <TwitterShareButton style={{ paddingLeft: "5px" }} url={`${Quotes} \n -${Author}- \n`} hashtags={["quotes", "quote", "katakatabijak"]}>
+          <TwitterIcon size={36} round />
+        </TwitterShareButton>
+
+        <WhatsappShareButton style={{ paddingLeft: "5px" }} title={`${Quotes}`} separator=":: ">
+          <WhatsappIcon size={36} round />
+        </WhatsappShareButton>
+
+        <TelegramShareButton style={{ paddingLeft: "5px" }} title={"Quotes"}>
+          <TelegramIcon size={36} round />
+        </TelegramShareButton>
       </div>
     </div>
   );
---
+++ src/components/shareButton.jsx
@@ -0,0 +1,7 @@
+const shareButton = () => {
+  <div className="button-container">
+    <h1>ini button</h1>
+  </div>;
+};
+
+export default shareButton;
